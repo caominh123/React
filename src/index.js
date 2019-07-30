50bd6eec43ca10d6b8b350a3b091614e1a23700c
= <=>
2019-07-30 22:08:58 +0700

    React Conditonnal

--- src/index.js
+++ src/index.js
@@ -101,21 +101,18 @@ ReactDOM.render(
 );
  */
 // TODO  Handling Events
-class Toggle extends React.Component {
+
+/* class Toggle extends React.Component {
     constructor(props) {
         super(props);
         this.state = { isToggleOn: true };
-
-
         this.handleClick = this.handleClick.bind(this);
     }
-
     handleClick() {
-        this.setState(prevState => ({
-            isToggleOn: !prevState.isToggleOn
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
         }));
     }
-
     render() {
         return (
             <button onClick={this.handleClick}>
@@ -124,8 +121,73 @@ class Toggle extends React.Component {
         );
     }
 }
+ReactDOM.render(
+    <Toggle />, document.getElementById('root')
+); */
+// TODO Conditional Rendering
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = { isLoggedIn: false };
+    }
+    handleLoginClick() {
+        this.setState({ isLoggedIn: true });
+    }
+    handleLogoutClick() {
+        this.setState({ isLoggedIn: false });
+    }
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick} />;
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick} />;
+        }
+
+        return (
+            <div>
+                <Greeting isLoggedIn={isLoggedIn} />
+                {button}
+            </div>
+        );
+    }
+}
+
+function UserGreeting(props) {
+    return <h1>Welcome Back!</h1>;
+}
+function GuestGreeting(props) {
+    return <h1>Please sign up!</h1>;
+}
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
 
 ReactDOM.render(
-    <Toggle />,
+    <LoginControl />,
     document.getElementById('root')
 );
+
+
