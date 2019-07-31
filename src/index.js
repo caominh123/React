d201c82910996684ff180ab4645d66026d5c1c20
= <=>
2019-07-31 18:28:21 +0700

    HOC

--- src/index.js
+++ src/index.js
@@ -224,11 +224,12 @@ ReactDOM.render(
     <Page />,
     document.getElementById('root')
 ); */
-function ListItems(props) {
+/*function ListItems(props) {
     return (
         <li>{props.value}</li>
     );
 }
+
 function NumberList(props) {
     const numbers = props.numbers;
     const listItems = numbers.map((numbers) =>
@@ -245,4 +246,171 @@ const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
     <NumberList numbers={numbers} />,
     document.getElementById('root')
-)
+)*/
+// TODO Accessibility ------------------------------------recode form here
+/* class BlurExample extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { isOpen: false };
+        this.timeOutId = null;
+
+        this.onClickHandler = this.onClickHandler.bind(this);
+        this.onBlurHandler = this.onBlurHandler.bind(this);
+        this.onFocusHandler = this.onFocusHandler.bind(this);
+    }
+
+    onClickHandler() {
+        this.setState(currentState => ({
+            isOpen: !currentState.isOpen
+        }));
+    }
+
+    // We close the popover on the next tick by using setTimeout.
+    // This is necessary because we need to first check if
+    // another child of the element has received focus as
+    // the blur event fires prior to the new focus event.
+    onBlurHandler() {
+        this.timeOutId = setTimeout(() => {
+            this.setState({
+                isOpen: false
+            });
+        });
+    }
+
+    // If a child receives focus, do not close the popover.
+    onFocusHandler() {
+        clearTimeout(this.timeOutId);
+    }
+
+    render() {
+        // React assists us by bubbling the blur and
+        // focus events to the parent.
+        return (
+            <div onBlur={this.onBlurHandler}
+                onFocus={this.onFocusHandler}>
+
+                <button onClick={this.onClickHandler}
+                    aria-haspopup="true"
+                    aria-expanded={this.state.isOpen}>
+                    Select an option
+          </button>
+                {this.state.isOpen && (
+                    <ul>
+                        <li>Option 1</li>
+                        <li>Option 2</li>
+                        <li>Option 3</li>
+                    </ul>
+                )}
+            </div>
+        );
+    }
+}
+ReactDOM.render(
+    < BlurExample />,
+    document.getElementById('root')
+) */
+// TODO Context ???
+// TODO ERROR
+/* class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { error: null, errorInfo: null };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        // Catch errors in any components below and re-render with error message
+        this.setState({
+            error: error,
+            errorInfo: errorInfo
+        })
+        // You can also log error messages to an error reporting service here
+    }
+
+    render() {
+        if (this.state.errorInfo) {
+            // Error path
+            return (
+                <div>
+                    <h2>Something went wrong.</h2>
+                    <details style={{ whiteSpace: 'pre-wrap' }}>
+                        {this.state.error && this.state.error.toString()}
+                        <br />
+                        {this.state.errorInfo.componentStack}
+                    </details>
+                </div>
+            );
+        }
+        // Normally, just render children
+        return this.props.children;
+    }
+}
+
+class BuggyCounter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0 };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(({ counter }) => ({
+            counter: counter + 1
+        }));
+    }
+
+    render() {
+        if (this.state.counter === 5) {
+            // Simulate a JS error
+            throw new Error('I crashed!');
+        }
+        return <h1 onClick={this.handleClick}>{this.state.counter}</h1>;
+    }
+}
+
+function App() {
+    return (
+        <div>
+            <p>
+                <b>
+                    This is an example of error boundaries in React 16.
+            <br /><br />
+                    Click on the numbers to increase the counters.
+            <br />
+                    The counter is programmed to throw when it reaches 5. This simulates a JavaScript error in a component.
+          </b>
+            </p>
+            <hr />
+            <ErrorBoundary>
+                <p>These two counters are inside the same error boundary. If one crashes, the error boundary will replace both of them.</p>
+                <BuggyCounter />
+                <BuggyCounter />
+            </ErrorBoundary>
+            <hr />
+            <p>These two counters are each inside of their own error boundary. So if one crashes, the other is not affected.</p>
+            <ErrorBoundary><BuggyCounter /></ErrorBoundary>
+            <ErrorBoundary><BuggyCounter /></ErrorBoundary>
+        </div>
+    );
+}
+
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+); */
+// TODO Fragment
+function Example() {
+    return (
+        <>
+            Some text.
+        <h2>A heading</h2>
+            <p>More text.</p>
+            <h2>Another heading</h2>
+            Even more text.
+      </>
+    );
+}
+ReactDOM.render(<Example />, document.getElementById('root'));
+
